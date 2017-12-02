ce6c5ee5b45740b71d0eee13432042b02e54537b
djreed <ddavidreed@gmail.com>
2017-12-02 17:50:59 -0500

    Release module

---
+++ lib/battleship/release.ex
@@ -0,0 +1,56 @@
+defmodule Battleship.ReleaseTasks do
+
+  @start_apps [
+    :crypto,
+    :ssl,
+    :postgrex,
+    :ecto
+  ]
+
+  @myapps [
+    :battleship
+  ]
+
+  @repos [
+    Battleship.Repo
+  ]
+
+  def seed do
+    IO.puts "Loading battleship.."
+    # Load the code for myapp, but don't start it
+    :ok = Application.load(:battleship)
+
+    IO.puts "Starting dependencies.."
+    # Start apps necessary for executing migrations
+    Enum.each(@start_apps, &Application.ensure_all_started/1)
+
+    # Start the Repo(s) for myapp
+    IO.puts "Starting repos.."
+    Enum.each(@repos, &(&1.start_link(pool_size: 1)))
+
+    # Run migrations
+    Enum.each(@myapps, &run_migrations_for/1)
+
+    # Run the seed script if it exists
+    seed_script = Path.join([priv_dir(:battleship), "repo", "seeds.exs"])
+    if File.exists?(seed_script) do
+      IO.puts "Running seed script.."
+      Code.eval_file(seed_script)
+    end
+
+    # Signal shutdown
+    IO.puts "Success!"
+    :init.stop()
+  end
+
+  def priv_dir(app), do: "#{:code.priv_dir(app)}"
+
+  defp run_migrations_for(app) do
+    IO.puts "Running migrations for #{app}"
+    Ecto.Migrator.run(Battleship.Repo, migrations_path(app), :up, all: true)
+  end
+
+  defp migrations_path(app), do: Path.join([priv_dir(app), "repo", "migrations"])
+  defp seed_path(app), do: Path.join([priv_dir(app), "repo", "seeds.exs"])
+
+end
---
+++ rel/commands/migrate.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+./bin/battleship command Elixir.Battleship.ReleaseTasks seed
+./bin/battleship command Elixir.Battleship.ReleaseTasks migrate
